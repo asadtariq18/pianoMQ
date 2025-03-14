7e8790c2d96bc26af878b23dcbae08008b4a5df7
asadtariq18 <asadtariq070@gmail.com>
2025-03-14 16:13:40 +0500

    Initial App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Index from './pages/Index';
+
+function App() {
+  return (
+    <div className="App">
+      <Index></Index>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Index() {
+  return (
+    <div>
+      <h1>Piano</h1>
+      <div>
+        <button className="text-primary">Red</button>
+      </div>
+    </div>
+  );
+}
